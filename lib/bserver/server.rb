2ad5848d983366a5d70f263f4d616af8943a9692
Andrey N. Shalaev <isqad88@yandex.ru>
2013-11-07 17:39:11 +0600

    file sending by sendfile(2)

--- lib/bserver/server.rb
+++ lib/bserver/server.rb
@@ -1,20 +1,52 @@
 # encoding: utf-8
 
+require 'rubygems'
+require 'bundler/setup'
+
 require 'webrick'
+require 'sendfile'
 
 # TODO: тут какие-то сорняки...
 require './lib/bserver/logger'
 require './lib/bserver/server_socket'
 require './lib/bserver/request_handler'
 
-=begin
-module WEBrick
-  # Public: MonkeyPatch - ускоряем отправку статики за счет внедрения sendfile(2) system call
-  class HTTPResponse
-    # TODO: зафигачить эту фичу
+# Public: MonkeyPatch - ускоряем отправку статики за счет внедрения sendfile(2) system call
+class WEBrick::HTTPResponse
+  # Public: данный метод выделен из webrick-high-performance и реализует функционал
+  # отправки файла через sendfile(2)
+  #
+  # socket - соединение с клиентом
+  def send_body_io(socket)
+    # это вредно. http://www.unixguide.net/network/socketfaq/2.16.shtml
+    #socket.setsockopt Socket::IPPROTO_TCP, Socket::TCP_NODELAY, 1 # HACK yuck
+
+    if @request_method == "HEAD"
+      # ничего не делаем
+    elsif @body.kind_of? File
+      # используем sendfile(2)
+      socket.sendfile @body
+    elsif chunked?
+      # буферизированный вывод
+      while buf = @body.read(BUFSIZE)
+        next if buf.empty?
+        data = ""
+        data << format("%x", buf.size) << CRLF
+        data << buf << CRLF
+        _write_data(socket, data)
+        @sent_size += buf.size
+      end
+      _write_data(socket, "0#{CRLF}#{CRLF}")
+    else
+      # тут оригинал
+      size = @header['content-length'].to_i
+      _send_file(socket, @body, 0, size)
+      @sent_size = size
+    end
+  ensure
+    @body.close
   end
 end
-=end
 
 module Bserver
   # Public: класс отвечающий за запуск сервера
